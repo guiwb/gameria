b6091f3a0c0980c64a92b11c167f779982f84200
Guilherme Barbosa <gui.wb@hotmail.com>
2020-12-14 22:19:03 -0300

    feat: show EditGame modal when user clicks on Card list

--- src/pages/Home.jsx
+++ src/pages/Home.jsx
@@ -1,6 +1,8 @@
 import styled from "styled-components";
 import Navbar from "../components/ui/Navbar";
 import Card from "../components/ui/Card";
+import EditGame from "../components/EditGame";
+import { useState } from "react";
 
 const Container = styled.div`
   display: flex;
@@ -28,17 +30,36 @@ const List = styled.div`
   grid-template-columns: 1fr 1fr 1fr;
 `;
 
-const Home = () => (
-  <Container>
-    <Navbar />
-    <Title>Meus jogos</Title>
-    <List>
-      <Card />
-      <Card />
-      <Card />
-      <Card />
-    </List>
-  </Container>
-);
+const Home = () => {
+  const [selectedGame, setSelectedGame] = useState(null);
+
+  return (
+    <Container>
+      <Navbar />
+      <Title>Meus jogos</Title>
+      <List>
+        <Card
+          onClick={() =>
+            setSelectedGame({
+              name: "Mario",
+              image: "",
+              category: "aventura",
+              rating: "5",
+            })
+          }
+        />
+        <Card />
+        <Card />
+        <Card />
+      </List>
+      {selectedGame && (
+        <EditGame
+          game={selectedGame}
+          clickedOut={() => setSelectedGame(null)}
+        />
+      )}
+    </Container>
+  );
+};
 
 export default Home;
