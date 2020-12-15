8a6164a75bc1abbc22bc6867cc78ccdfdca142bb
Guilherme Barbosa <gui.wb@hotmail.com>
2020-12-15 01:09:20 -0300

    feat: display user profile

--- src/components/ui/Navbar/Profile.jsx
+++ src/components/ui/Navbar/Profile.jsx
@@ -22,6 +22,10 @@ const Name = styled.p`
   margin: 0 20px;
   font-size: 16px;
   color: #fff;
+
+  b {
+    text-transform: capitalize;
+  }
 `;
 
 const Profile = () => {
@@ -36,10 +40,14 @@ const Profile = () => {
     }
   };
 
+  const user = auth.currentUser;
+
   return (
     <Wrapper>
-      <Picture src="https://scontent.fpoa8-1.fna.fbcdn.net/v/t1.0-9/74352081_2228318253940054_1604344175396913152_n.jpg?_nc_cat=100&ccb=2&_nc_sid=a4a2d7&_nc_eui2=AeFaJB5HF68VFuX4rFZNJYeYzeAvrbHnI8zN4C-tsecjzAKSnqR_HhpFZPQxXg8UBKBz5kjE55y9b_AUhKOLPwgY&_nc_ohc=viJTlSujVk0AX_7lwUg&_nc_ht=scontent.fpoa8-1.fna&oh=5667e016a642221d32933cf78299efbf&oe=5FFD8488" />
-      <Name>Seja bem-vindo, Guilherme</Name>
+      <Picture src={user.photoURL} />
+      <Name>
+        Seja bem-vindo, <b>{user.displayName}</b>
+      </Name>
       <Button onClick={logout} color="red">
         logout
       </Button>
