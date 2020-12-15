7c0c79708fdb06d4b1874672deb470172d12dc02
Guilherme Barbosa <gui.wb@hotmail.com>
2020-12-14 22:34:30 -0300

    feat: add direction prop to form

--- src/components/ui/Form.jsx
+++ src/components/ui/Form.jsx
@@ -2,6 +2,6 @@ import styled from "styled-components";
 
 export default styled.form`
   display: flex;
-  flex-direction: column;
+  flex-direction: ${({ direction = "column" }) => direction};
   align-items: center;
 `;
