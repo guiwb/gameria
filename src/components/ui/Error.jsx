df8ef2a5ee8647d2291d6adfc71a92f6254c55f3
Guilherme Barbosa <gui.wb@hotmail.com>
2020-12-14 22:27:31 -0300

    feat: create error component

---
+++ src/components/ui/Error.jsx
@@ -0,0 +1,13 @@
+import styled from "styled-components";
+
+const ListError = styled.div`
+  width: 100%;
+  padding: 50px 0;
+  text-align: center;
+  font-size: 22px;
+  text-transform: uppercase;
+  font-weight: bolder;
+  color: rgba(255, 255, 255, 0.4);
+`;
+
+export default ({ children }) => <ListError>{children}</ListError>;
