7a013632da66ade4406760f65501fc873ca1e1bb
Guilherme Barbosa <gui.wb@hotmail.com>
2020-12-15 01:15:14 -0300

    feat: remove toLowerCase from register name

--- src/pages/Register.jsx
+++ src/pages/Register.jsx
@@ -19,9 +19,7 @@ const Register = (props) => {
     e.preventDefault();
 
     const photoURL = document.querySelector("input[name=picture]").value;
-    const displayName = document
-      .querySelector("input[name=name]")
-      .value.toLowerCase();
+    const displayName = document.querySelector("input[name=name]");
     const email = document.querySelector("input[type=email]").value;
     const password = document.querySelector("input[type=password]").value;
 
