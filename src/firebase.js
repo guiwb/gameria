27d6fa638f28b495708246fffb6d794d41c5430c
Guilherme Barbosa <gui.wb@hotmail.com>
2020-12-14 23:42:33 -0300

    feat: intialize firebase

---
+++ src/firebase.js
@@ -0,0 +1,18 @@
+import firebase from "firebase/app";
+import "firebase/database";
+import "firebase/auth";
+
+const firebaseConfig = {
+  apiKey: "AIzaSyD2VyB3IVhfsh8wqtHZjfpcKuFGUMUGfG4",
+  authDomain: "gameria-bda.firebaseapp.com",
+  databaseURL: "https://gameria-bda-default-rtdb.firebaseio.com",
+  projectId: "gameria-bda",
+  storageBucket: "gameria-bda.appspot.com",
+  messagingSenderId: "561890909364",
+  appId: "1:561890909364:web:2cdedbbbaf0614139e0be1",
+};
+
+firebase.initializeApp(firebaseConfig);
+
+export const gamesRef = firebase.database().ref("games");
+export const user = firebase.auth().currentUser;
